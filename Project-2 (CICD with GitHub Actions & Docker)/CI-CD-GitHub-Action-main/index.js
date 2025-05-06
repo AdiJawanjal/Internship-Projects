328fd6dd699ff059f278a62d2e3bb1f9ab218c12
Aditya Jawanjal <97746325+AdiJawanjal@users.noreply.github.com>
2025-05-07 01:24:29 +0530

    Add files via upload

---
+++ Project-2 (CICD with GitHub Actions & Docker)/CI-CD-GitHub-Action-main/index.js
@@ -0,0 +1,10 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline!'));
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
